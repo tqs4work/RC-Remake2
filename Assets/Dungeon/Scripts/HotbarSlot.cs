e5c76c782013199500905e04d56bdc71ab471ccd
Son <tqs4work@gmail.com>
2026-03-01 15:21:01 +0700

    1

--- Assets/Dungeon/Scripts/HotbarSlot.cs
+++ Assets/Dungeon/Scripts/HotbarSlot.cs
@@ -1,46 +1,3 @@
-//using UnityEngine;
-//using UnityEngine.UI;
-//using TMPro;
-
-//public class HotbarSlot : MonoBehaviour
-//{
-//    public Image icon;
-//    public TextMeshProUGUI amountText;
-//    public GameObject highlight;
-
-//    private ItemRuntime currentItem;
-
-//    public void SetItem(ItemRuntime item)
-//    {
-//        currentItem = item;
-
-//        if (item == null)
-//        {
-//            icon.sprite = null;
-//            icon.enabled = false;
-//            amountText.text = "";
-//            return;
-//        }
-
-//        icon.enabled = true;
-//        icon.sprite = item.icon;
-
-//        amountText.text = item.isStackable && item.quantity > 1
-//            ? item.quantity.ToString()
-//            : "";
-//    }
-
-//    public ItemRuntime GetItem()
-//    {
-//        return currentItem;
-//    }
-
-//    public void SetHighlight(bool state)
-//    {
-//        highlight.SetActive(state);
-//    }
-//}
-
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -94,13 +51,32 @@ public class HotbarSlot : MonoBehaviour, IPointerClickHandler
         {
             var player = PlayerRuntime.Instance.Player;
 
+            string itemID = currentItem.itemID;
+
             player.MoveFromHotbar(
                 slotIndex,
-                InventoryContainerType.Tool
+                GetContainerType(itemID)
             );
 
             FindFirstObjectByType<HotbarUI>()?.Refresh();
             FindFirstObjectByType<InventoryUI>()?.RefreshAll();
         }
+    }
+
+    private InventoryContainerType GetContainerType(string itemID)
+    {
+        if (string.IsNullOrEmpty(itemID))
+            return InventoryContainerType.Tool;
+
+        char prefix = itemID[0];
+
+        return prefix switch
+        {
+            'T' => InventoryContainerType.Tool,
+            'F' => InventoryContainerType.Farm,
+            'C' => InventoryContainerType.City,
+            'D' => InventoryContainerType.Dungeon,
+            _ => InventoryContainerType.Tool
+        };
     }
 }
