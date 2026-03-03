4dc90629372de9ac197d50ff5f6416922890e238
Trang <duongthituyettrang2407@gmail.com>
2026-03-04 01:44:40 +0700

    update

--- Assets/Farm/Scripts_Farm/Animal/ChicManager.cs
+++ Assets/Farm/Scripts_Farm/Animal/ChicManager.cs
@@ -57,6 +57,10 @@ public class ChicManager : MonoBehaviour
     private Vector3 startPos; // Lưu vị trí lúc mới sinh ra để gà không đi quá xa
     private bool isWalking = false;
 
+    public AudioSource AudioSource;
+    public AudioClip eatSound;
+    public Transform chicVisuals;
+
     private void Start()
     {
         //Khởi tạo timer cho trạng thái baby
@@ -74,28 +78,36 @@ public class ChicManager : MonoBehaviour
     }
     private void Update()
     {
-        if (currentState == ChicState.Baby && !isHungry)
+        if (currentState == ChicState.Baby)
         {
-            hungerTimer -= Time.deltaTime;
-            if (hungerTimer <= 0f)
+            if (isHungry)
             {
-                isHungry = true;
-                Debug.Log("Gà đói rồi! Hãy cho ăn!");
-                Interact.gameObject.SetActive(true);
-                food.enabled = true;
-                sellIcon.enabled = false;
+                ShowHungryUI();
+            }
+            else
+            {
+                hungerTimer -= Time.deltaTime;
+                if (hungerTimer <= 0f)
+                {
+                    isHungry = true;
+                    Debug.Log("Gà đói rồi! Hãy cho ăn!");
+                    
+                }
             }
         }
-        else if (currentState == ChicState.Teen && !isTeenHungry)
+        else if (currentState == ChicState.Teen)
         {
-            teenHungerTimer -= Time.deltaTime;
-            if (teenHungerTimer <= 0f)
+            if (isTeenHungry)
             {
-                Interact.gameObject.SetActive(true);
-                food.enabled = true;
-                sellIcon.enabled = false;
-                isTeenHungry = true;
-                Debug.Log("Gà teen đói rồi! Hãy cho ăn!");
+                ShowHungryUI();
+            }
+            else
+            {
+                teenHungerTimer -= Time.deltaTime;
+                if (teenHungerTimer <= 0f)
+                {
+                    isTeenHungry = true;
+                }
             }
         }
     }
@@ -136,8 +148,31 @@ public class ChicManager : MonoBehaviour
                 }
         }    
     }
+    private bool Feed()
+    {
+        var player = PlayerRuntime.Instance.Player;
+        var farmInventory = player.Inventory[InventoryContainerType.Farm];
+        var wheatItem = farmInventory.items.Find(x => x.itemID == "F1"); //  ID của thức ăn wheat là F1
+        if (wheatItem == null || wheatItem.quantity <= 0)
+        {
+            Debug.Log("Bạn không có thức ăn để cho gà con ăn!");
+            return false;
+        }
+        wheatItem.quantity--; // Trừ 1 thức ăn
+        Debug.Log("Đã sử dụng 1 thức ăn. Còn lại: " + wheatItem.quantity);
+        if (wheatItem.quantity <= 0)
+        {
+            farmInventory.items.Remove(wheatItem); // Xóa khỏi túi nếu hết
+            Debug.Log("Bạn đã dùng hết thức ăn!");
+            return false;
+        }
+        return true;
+    }    
     private void FeedBaby()
     {
+        if (!Feed()) return;
+        AudioSource.PlayOneShot(eatSound);
+        Debug.Log("Sound of chic!");
         isHungry = false;
         hungerTimer = hungerInterval; // Reset lại đồng hồ đếm đói
         timesFed++;
@@ -161,6 +196,9 @@ public class ChicManager : MonoBehaviour
     }
     private void FeedTeen()
     {
+        if (!Feed()) return;
+        AudioSource.PlayOneShot(eatSound);
+        Debug.Log("Sound of chic!");
         isTeenHungry = false;
         teenHungerTimer = teenHungerInterval; // Reset lại đồng hồ đếm đói
         teenTimesFed++;
@@ -248,11 +286,11 @@ public class ChicManager : MonoBehaviour
             // Chú ý: Giả định model gốc của bạn đang quay mặt sang phải. X = 1 là phải, -1 là trái.
             if (targetPos.x < transform.position.x)
             {
-                transform.localScale = new Vector3(-1, 1, 1); // Lật sang trái
+                chicVisuals.localScale = new Vector3(-1, 1, 1); // Lật sang trái
             }
             else if (targetPos.x > transform.position.x)
             {
-                transform.localScale = new Vector3(1, 1, 1); // Lật sang phải
+                chicVisuals.localScale = new Vector3(1, 1, 1); // Lật sang phải
             }
 
             // 4. Bắt đầu di chuyển từ từ đến targetPos
