b89bdf19df3d9283eac1df52f82d0dcf4c6450ba
GiaHuy1106 <giahuy110600@gmail.com>
2026-03-03 13:54:41 +0700

    + add new assets

---
+++ Assets/City/City_Script/AnimTrigger.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class AnimTrigger : MonoBehaviour
+{
+    public SpriteSwap DoorOpen;
+
+    private bool playerInRange;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            playerInRange = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            playerInRange = false;
+        DoorOpen.CloseDoor();
+    }
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(DoorOpen.OpenDoor());
+        }
+    }
+}
---
+++ Assets/Scripts/Dice Scripts/ScoreTrigger.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTrigger : MonoBehaviour
+{
+    Dice1Scripts dice;
+    Dice2Scripts dice2;
+    Dice3Scripts dice3;
+    [SerializeField] private Text ScoreText;
+
+    private void Awake()
+    {
+        dice = FindAnyObjectByType<Dice1Scripts>();    
+        dice2 = FindAnyObjectByType<Dice2Scripts>();
+        dice3 = FindAnyObjectByType<Dice3Scripts>();
+    }
+
+    private void Update()
+    {
+        ScoreCheck();
+    }
+
+    public void ScoreCheck()
+    {
+        if (dice != null && dice2 != null && ScoreText != null)
+        {
+            if (dice.diceFaceNum != 0 && dice2.diceFaceNum != 0 && dice3.diceFaceNum != 0)
+            {
+                int tong = dice.diceFaceNum + dice2.diceFaceNum + dice3.diceFaceNum;
+                ScoreText.text = tong.ToString();
+
+                if (tong >= 3 && tong <= 10)
+                {
+                    
+                }
+                else if (tong >= 11 && tong <= 18)
+                {
+
+                }
+
+                if (BettingManager.Instance != null)
+                {
+                    BettingManager.Instance.CheckResult(tong);
+                }
+            }
+            
+        }      
+    
+    }
+}
